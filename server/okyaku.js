e5b81ceb1bd023da5a2c961138b2606493c63e71
bakesta <>
2017-07-11 22:50:43 -0400

    Removed part of scan_client_caps that was causing users to get dropped every time hot_reload() was called
    This will probably cause a desync problem somewhere, but I cant find it right now
    
    Signed-off-by: yumi <>

--- server/okyaku.js
+++ server/okyaku.js
@@ -165,12 +165,6 @@ exports.scan_client_caps = function () {
 				okyaku.blackhole = true;
 				return;
 			}
-			if (!caps.can_access_board(ident, okyaku.board)) {
-				try {
-					okyaku.socket.close();
-				}
-				catch (e) { /* bleh */ }
-			}
 		});
 	}
 };
